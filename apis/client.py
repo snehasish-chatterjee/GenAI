6ac06d96c0f62420ae64c2843bba1ebb410f71ac
Snehasish Chatterjee <snehasishchatterjee90@gmail.com>
2025-06-23 18:37:10 +0530

    commit

---
+++ apis/client.py
@@ -0,0 +1,12 @@
+import requests
+import streamlit as st
+
+def get_cohere_response(input_text):
+    response = requests.post("http://localhost:8000/essay/invoke", json={"topic": input_text})
+    return response.json()
+
+st.title("Cohere Essay Generator")
+input_text = st.text_input("Enter a topic for the essay:")
+
+if input_text:
+    st.write(get_cohere_response(input_text))
